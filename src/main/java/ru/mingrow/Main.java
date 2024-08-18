a4205e0d1171fd0518655ff4c8731fa24cb332e0
mingrow <nikitosvozh@gmail.com>
2024-08-18 12:23:24 +0300

    Add: SessionFactory

--- src/main/java/ru/mingrow/Main.java
+++ src/main/java/ru/mingrow/Main.java
@@ -1,7 +1,12 @@
 package ru.mingrow;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try (SessionFactory sf = new Configuration().configure().buildSessionFactory()) {
+            System.out.println("OK");
+        }
     }
 }
